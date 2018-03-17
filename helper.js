9148b9927fd4c310bdf6e24d008cf3cebdf97875
Daniel Rothblatt <drothblatt@tworiverstrading.com>
2018-03-17 12:28:13 -0400

    [refactor] Implement zipObject with Array.prototype.reduce

--- helper.js
+++ helper.js
@@ -1,17 +1,15 @@
 const combineTables = (...tables) => Object.assign({}, ...tables);
-const compose = (...fns) => fns.reduce(compose2, identity);
 const compose2 = (f, g) => x => f(g(x));
+const compose = (...fns) => fns.reduce(compose2, identity);
 const flip = (f) => (x, y) => f(y,x);
 const identity = (x) => x;
 const partial = (f, ...args) => (...moreArgs) => f(...args, ...moreArgs);
 const zip = (xs, ys) => xs.map((x, i) => [x, ys[i]]);
-const zipObject = (xs, ys) => {
-    const obj = {};
-    zip(xs, ys)
-        .forEach(([x, y]) => obj[x] = y);
-
-    return obj;
-};
+const zipObject = (xs, ys) =>
+      zip(xs, ys).reduce((obj, [k, v]) => {
+          obj[k] = v;
+          return obj;
+      }, {});
 
 module.exports = {
     combineTables,
