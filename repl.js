20ee2c079d43bfbce826a4bf66298d1ba2e2fdc9
Daniel Rothblatt <drothblatt@tworiverstrading.com>
2018-03-17 15:27:12 -0400

    [lookup-tables] Add inequality comparisons

--- repl.js
+++ repl.js
@@ -1,9 +1,10 @@
-const arithmeticTable     = require('./arith.js');
-const consTable           = require('./cons.js');
+const arithmeticTable     = require('./tables/arith.js');
+const consTable           = require('./tables/cons.js');
+const truthTable          = require('./tables/truth.js');
 const { makeInterpreter } = require('./slip.js');
 
 //--------------------------------------------------------------------------------
 
 const stdin = process.openStdin();
-const interpreter = makeInterpreter(arithmeticTable, consTable);
+const interpreter = makeInterpreter(arithmeticTable, consTable, truthTable);
 stdin.addListener('data', d => console.log(interpreter(d.toString().trim())));
