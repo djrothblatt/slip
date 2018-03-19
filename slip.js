c88e2b2e06cfb2d2c8f25fa24c5edb186f482136
Daniel Rothblatt <drothblatt@tworiverstrading.com>
2018-03-19 07:49:51 -0400

    [refactor] Call operator directly

--- slip.js
+++ slip.js
@@ -73,7 +73,7 @@ const evalSexp = (sexp, table={}) => {
         // function call
         const operator = evalSexp(car, table);
         const operands = cdr.map(rand => evalSexp(rand, table));
-        return operator.call(null, ...operands);
+        return operator(...operands);
     }
 
     return table[sexp] || sexp;
