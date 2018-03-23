387a41b68eedf5d03e0b7ee3b14c910c58c9f0b1
Daniel Rothblatt <drothblatt@tworiverstrading.com>
2018-03-23 08:05:52 -0400

    [refactor] Allow multiple arguments to composed functions

--- helper.js
+++ helper.js
@@ -1,6 +1,6 @@
 const symb = (val) => typeof val === 'string' && val[0] !== '"' ? Symbol.for(val) : val;
 const combineTables = (...tables) => Object.assign({}, ...tables);
-const compose2 = (f, g) => x => f(g(x));
+const compose2 = (f, g) => (...x) => f(g(...x));
 const compose = (...fns) => fns.reduce(compose2, identity);
 const flip = (f) => (x, y) => f(y,x);
 const identity = (x) => x;
